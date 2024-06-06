97e54dc5dc3f4539b75b4eb6b3f9b31613f09fa6
JarlM06 <jarl.ebbesen@outlook.com>
2024-06-06 08:06:20 +0000

    PHP fil som kobler seg til databasen og henter dataen som ligger der

---
+++ Backend/getData.php
@@ -0,0 +1,44 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Login informasjon om MySQL server
+$servername = "172.20.128.21";
+$username = "jm";
+$password = "Akademiet99!";
+$database = "bokregister";
+
+// Etablerer en kobling til MySQL serveren
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Sjekker tilkoblingen
+if ($conn -> connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Henter all data fra "boker" i databasen
+$sql = "SELECT Tittel, Forfattere, ISBN, Utgiver, Publiseringsar, Sideantall FROM boker";
+$result = $conn->query($sql);
+
+if ($result === FALSE) {
+    die("Error executing query: " . $conn->error);
+}
+
+if ($result->num_rows > 0) {
+    // Returnerer dataen av hver rad som JSON
+    $rows = array();
+    while ($row = $result->fetch_assoc()) {
+        $rows[] = $row;
+    }
+    header('Content-Type: application/json');
+    echo json_encode($rows);
+} else {
+    header('Content-Type: application/json');
+    echo json_encode(array()); // Returnerer et tomt array hvis ingen data er funnet
+}
+
+$conn->close();
+
+?>
